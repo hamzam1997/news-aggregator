ead928f724191ca7136e26fce1eda181bd147778
Hamza Masroor <hamza-masroor@hotmail.com>
2020-07-02 06:17:28 +0500

    Implemented part I for both reddit and newsAPI

---
+++ news_aggregator/news/ThirdPartyAPIFunctions.py
@@ -0,0 +1,42 @@
+from newsapi import NewsApiClient
+import praw
+
+
+newsapi = NewsApiClient("6fc8fbebbb8d48859431e23e20ef264b")
+
+reddit = praw.Reddit(client_id='ea4HN792BzUdOQ', \
+                     client_secret='zH-1JdgfVEv1Xg1KctjAi9KvG28', \
+                     user_agent='news-aggregator'
+                     )
+
+def getPostsFromNewsAPI(query = None):
+    top_headlines = newsapi.get_top_headlines(q = query, category = 'general')
+    cleanedArticles = []
+    for i in top_headlines["articles"]:
+        news = {"headline":i["title"],
+                "link": i["url"],
+                "source":"newsapi"
+                }
+        cleanedArticles.append(news)
+    return cleanedArticles
+
+
+def getPostsFromReddit(query = None):
+    posts = None
+    if query in [None, ""]:
+        posts = reddit.subreddit('news').hot(limit = 100)
+    else:
+        posts = reddit.subreddit('news').search(query)
+
+    
+    cleanedPosts = []
+    for post in posts:
+        news = {"headline": post.title,
+                "link": post.url,
+                "source":"reddit"
+                }
+        cleanedPosts.append(news)
+    return cleanedPosts
+
+def getPosts(query = None):
+    return getPostsFromNewsAPI() + getPostsFromReddit()
--- news_aggregator/news/views.py
+++ news_aggregator/news/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
+from .ThirdPartyAPIFunctions import getPosts
 # Create your views here.
+
+def getNews(request):
+    query = request.GET.get('query')
+    response = getPosts()
+    return JsonResponse(response, safe = False)
