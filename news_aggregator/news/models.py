b224bee46dd69c01ccf9429002e7022a14d9a581
Hamza Masroor <hamza-masroor@hotmail.com>
2020-07-03 02:17:10 +0500

    Implemented models to store queries

--- news_aggregator/news/models.py
+++ news_aggregator/news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Query(models.Model):
+    query = models.CharField(max_length = 100)
+    queriedAt = models.DateTimeField(auto_now = True)
+
+class QueryResult(models.Model):
+    headline = models.CharField(max_length = 1000)
+    url = models.URLField()
+    source = models.CharField(max_length = 30)
+    queries = models.ManyToManyField(Query)
